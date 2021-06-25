1cf0daf51e64c527e79f3340e5fed9e91e670ea9
AntaripChat <85601268+AntaripChat@users.noreply.github.com>
2021-06-25 09:18:42 +0530

    Add files via upload
    first project

---
+++ camera.py
@@ -0,0 +1,11 @@
+import cv2 
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#eye_cascade = cv2.C
+img = cv2.imread('img.jpg',1)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+for (x,y,w,h) in faces:
+    cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,0),thickness=2)
+
+cv2.imshow('img',img)
+cv2.waitKey()
