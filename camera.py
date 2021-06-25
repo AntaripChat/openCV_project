3db2a00dd1ccd5a8176b734b3d7fd913ed82effa
AntaripChat <85601268+AntaripChat@users.noreply.github.com>
2021-06-25 09:28:26 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,5 +1,5 @@
 import cv2 
-face_cascade = cv2.CascadeClassifier('harr.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 #eye_cascade = cv2.C
 img = cv2.imread('img.jpg',1)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
